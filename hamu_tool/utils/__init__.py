322b771de27810b2317926aa16260e932a0d136e
waylight3 <waylight3@snu.ac.kr>
2024-05-13 01:09:15 +0000

    add stat funcs

--- hamu_tool/utils/__init__.py
+++ hamu_tool/utils/__init__.py
@@ -1,6 +1,3 @@
 from .bib_extractor import BIBExtractor
 from .corpus_reader import CorpusReader
-from .numeric import is_float
-from .numeric import is_int
-from .numeric import is_number
 from .pprint import pprint
--- hamu_tool/utils/numeric.py
+++ hamu_tool/utils/numeric.py
@@ -38,3 +38,54 @@ def is_number(x : str) -> bool:
         bool: True if the string is a number, False otherwise.
     """
     return is_int(x) or is_float(x)
+
+def mean(data : list) -> float:
+    """Calculate the mean of a list of numbers.
+
+    Args:
+        data (list): A list of numbers.
+
+    Returns:
+        float: The mean of the numbers.
+    """
+    return sum(data) / len(data)
+
+def variance(data : list) -> float:
+    """Calculate the variance of a list of numbers.
+
+    Args:
+        data (list): A list of numbers.
+
+    Returns:
+        float: The variance of the numbers.
+    """
+    m = mean(data)
+    return mean([(x - m) ** 2 for x in data])
+
+def std_dev(data : list) -> float:
+    """Calculate the standard deviation of a list of numbers.
+
+    Args:
+        data (list): A list of numbers.
+
+    Returns:
+        float: The standard deviation of the numbers.
+    """
+    return variance(data) ** 0.5
+
+def median(data : list) -> float:
+    """Calculate the median of a list of numbers.
+
+    Args:
+        data (list): A list of numbers.
+
+    Returns:
+        float: The median of the numbers.
+    """
+    data = sorted(data)
+    n = len(data)
+    if n % 2 == 1:
+        return data[n // 2]
+    else:
+        return (data[n // 2 - 1] + data[n // 2]) / 2
+
