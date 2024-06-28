2285f27cfb25ff28a221fcc585bef5796bad1a90
waylight3 <waylight3@snu.ac.kr>
2024-06-28 06:16:26 +0000

    upgrade to version 0.0.16

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='hamu-tool',
-    version='0.0.15',
+    version='0.0.16',
     description='Many useful tools for computer scientists!',
     license='MIT',
     author='DoHyeon Lee',
